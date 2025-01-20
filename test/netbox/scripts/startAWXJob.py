e575c4db4cd43eddc5635e597dc58ae5e555a98b
Nockternal <burger@ru.co.za>
2025-01-20 01:51:21 +0100

    update

--- test/netbox/scripts/startAWXJob.py
+++ test/netbox/scripts/startAWXJob.py
@@ -1,4 +1,5 @@
 import requests
+from extras.scripts import *
 
 url = "http://192.168.127.162:30080/api/v2/job_templates/11/launch/"
 
@@ -12,3 +13,26 @@ headers = {
 response = requests.request("POST", url, headers=headers, data=payload)
 
 print(response.text)
+
+
+
+class Test(Script):
+    class Meta:
+        name = "Test"
+        description = "Test script"
+        commit_default = False
+
+    def run(self, data, commit):
+        print("Test script")
+        url = "http://192.168.127.162:30080/api/v2/job_templates/11/launch/"
+
+        payload = {}
+        headers = {
+        'Accept': 'application/json',
+        'Authorization': 'Bearer fSISbYdZUNZ2N2QMPHO5Ahq2YjP4bt',
+        'Cookie': 'csrftoken=tkHyX0vNLGCTIrXbCoWwq1AgOZj4VZEC'
+        }
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+
+        return response.text
